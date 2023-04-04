1f4cb07125861ce069ca65330ab82b3b4b6c6939
nathaniel-au <123103058+nathaniel-au@users.noreply.github.com>
2023-04-04 08:48:13 -0700

    Restore '4/4/2023, 8:42:47 AM Update pxt.json, main.blocks, main.ts'

--- main.py
+++ main.py
@@ -143,124 +143,13 @@ statusbars.on_zero(StatusBarKind.health, on_on_zero)
 def on_on_overlap(sprite2, otherSprite):
     global chosen_enemy
     chosen_enemy = otherSprite
+    pause(500)
     player1Health.value += -1
     player2Health.value += -1
-    animation.run_image_animation(otherSprite,
-        [img("""
-                . . . . c c c c c c c . . . . . 
-                        . . . c 6 7 7 7 7 7 6 c . . . . 
-                        . . c 6 7 c 6 6 6 6 c 7 c . . . 
-                        . . c 7 7 6 f 6 6 f 6 7 6 c . . 
-                        . . c 7 7 7 7 7 7 7 7 7 7 c . . 
-                        . . f 7 7 7 6 1 f f 1 8 7 f . . 
-                        . . f 7 7 7 f 1 f f 1 f 6 f . . 
-                        . . f 6 7 7 f 2 2 2 2 f f . . . 
-                        . . c f 6 7 7 2 2 2 2 f c c . . 
-                        . c 7 7 c c 7 7 7 7 7 7 7 7 c . 
-                        c 7 7 7 6 c f 7 7 7 7 1 1 1 7 c 
-                        c c 6 6 6 c c f 6 7 1 1 1 1 1 f 
-                        . . c 6 6 6 c 6 6 1 1 1 1 1 1 f 
-                        . . c 6 6 6 6 6 6 1 1 1 1 1 6 f 
-                        . . . c 6 6 6 6 1 1 1 1 1 6 f . 
-                        . . . . c c c c c c c c f f . .
-            """),
-            img("""
-                . . . c c c c c c c . . . . . . 
-                        . . c 7 f f 6 6 f f c . . . . . 
-                        . c 6 7 6 6 6 6 6 6 7 c . . . . 
-                        . c 7 7 7 7 7 7 7 7 7 7 c . . . 
-                        . c 7 7 7 6 1 f f 1 8 7 c . . . 
-                        . f 7 7 7 f 1 f f 1 f 6 f . . . 
-                        . f 7 7 7 f 2 2 2 2 f 6 f . . . 
-                        . f 6 7 7 f 2 2 2 2 f 6 c c . . 
-                        . . c f 7 7 2 2 2 2 7 7 7 7 c . 
-                        . c 7 7 c c 7 7 7 7 7 1 1 1 7 c 
-                        c 7 7 7 6 c f 7 7 7 1 1 1 1 1 f 
-                        c c 6 6 6 c c f 6 1 1 1 1 1 1 f 
-                        . . c 6 6 6 c 6 6 1 1 1 1 1 6 f 
-                        . . c 6 6 6 6 6 6 1 1 1 1 1 6 f 
-                        . . . c 6 6 6 6 6 1 1 1 1 6 f . 
-                        . . . . c c c c c c c c f f . .
-            """),
-            img("""
-                . . . c c c c c c c . . . . . . 
-                        . . c 7 f f 6 6 f f c . . . . . 
-                        . c 6 7 6 6 6 6 6 6 7 c . . . . 
-                        . c 7 7 7 7 7 7 7 7 7 7 c . . . 
-                        . c 7 7 7 6 1 f f 1 8 7 c . . . 
-                        . f 7 7 7 f 1 f f 1 f 6 f . . . 
-                        . f 7 7 7 f 2 2 2 2 f 6 f . . . 
-                        . f 6 7 7 f 2 2 2 2 f 6 c c . . 
-                        . . c f 7 7 2 2 2 2 7 7 7 7 c . 
-                        . c 7 7 c c 7 7 7 7 7 1 1 1 7 c 
-                        c 7 7 7 6 c f 7 7 7 1 1 1 1 1 f 
-                        c c 6 6 6 c c f 6 1 1 1 1 1 1 f 
-                        . . c 6 6 6 c 6 6 1 1 1 1 1 6 f 
-                        . . c 6 6 6 6 6 6 1 1 1 1 1 6 f 
-                        . . . c 6 6 6 6 6 1 1 1 1 6 f . 
-                        . . . . c c c c c c c c f f . .
-            """),
-            img("""
-                . . . . . . . . . . . . . . . . 
-                        . . . . . . . . . . . . . . . . 
-                        c c c c c . . . . . . . . . . . 
-                        c 6 7 7 7 c c . . . . . . . . . 
-                        . c c 7 7 7 c c . . . . . . . . 
-                        . . . c 7 7 6 c . . . . . . . . 
-                        . . . c 6 6 6 c . . . . . . . . 
-                        . . c c 6 6 6 c c c c c c . . . 
-                        . c 6 6 6 c c 6 7 7 7 7 6 c . . 
-                        c c 6 6 6 c 7 7 7 7 7 7 7 7 c . 
-                        c 6 6 6 c 6 7 7 7 7 7 7 7 7 6 c 
-                        c 6 6 6 c 7 7 7 c 6 6 6 6 c 7 c 
-                        c 6 6 6 f 7 7 7 c c 6 6 c c 7 f 
-                        c 6 6 6 f 7 7 7 6 f 6 6 f 6 7 f 
-                        . c c 6 6 f 6 7 c 1 f f c 1 c . 
-                        . . . c c c c c c c c c c c c .
-            """),
-            img("""
-                c c c c c . . . . . . . . . . . 
-                        c 6 7 7 7 c c . . . . . . . . . 
-                        . c c 7 7 7 c c . . . . . . . . 
-                        . . . c 7 7 6 c . . . . . . . . 
-                        . . . c 6 6 6 c . . . . . . . . 
-                        . . c c 6 6 6 c . . . . . . . . 
-                        . c c 6 6 6 c c c c c c c . . . 
-                        . c 6 6 6 c c 6 7 7 7 7 6 c . . 
-                        c c 6 6 6 c 7 7 7 7 7 7 7 7 c . 
-                        c 6 6 6 c 6 7 7 7 7 7 7 7 7 6 c 
-                        c 6 6 6 c 7 7 7 c 6 6 6 6 c 7 c 
-                        c 6 6 6 f 7 7 7 c c 6 6 c c 7 f 
-                        c 6 6 6 f 7 7 7 6 f 6 6 f 6 7 f 
-                        . c 6 6 f 6 7 7 7 7 7 7 7 7 f . 
-                        . c c 6 6 f 6 7 c 1 f f c 1 c . 
-                        . . . c c c c c c c c c c c c .
-            """),
-            img("""
-                c c c c c . . . . . . . . . . . 
-                        c 6 7 7 7 c c . . . . . . . . . 
-                        . c c 7 7 7 c c . . . . . . . . 
-                        . . . c 7 7 6 c . . . . . . . . 
-                        . . . c 6 6 6 c . . . . . . . . 
-                        . . c c 6 6 6 c . . . . . . . . 
-                        . c c 6 6 6 c c c c c c c . . . 
-                        . c 6 6 6 c c 6 7 7 7 7 6 c . . 
-                        c c 6 6 6 c 7 7 7 7 7 7 7 7 c . 
-                        c 6 6 6 c 6 7 7 7 7 7 7 7 7 6 c 
-                        c 6 6 6 c 7 7 7 c 6 6 6 6 c 7 c 
-                        c 6 6 6 f 7 7 7 c c 6 6 c c 7 f 
-                        c 6 6 6 f 7 7 7 6 f 6 6 f 6 7 f 
-                        . c 6 6 f 6 7 7 7 7 7 7 7 7 f . 
-                        . c c 6 6 f 6 7 c 1 f f c 1 c . 
-                        . . . c c c c c c c c c c c c .
-            """)],
-        100,
-        False)
-    pause(1000)
+    pause(500)
 sprites.on_overlap(SpriteKind.player, SpriteKind.enemy, on_on_overlap)
 
 # This block chain creates the players, map, and enemy
-Snake: Sprite = None
 Enemies: List[Sprite] = []
 chosen_enemy: Sprite = None
 p1Distance = 0
@@ -292,81 +181,6 @@ player1 = sprites.create(img("""
             . . . . . f f . . f f . . . . .
     """),
     SpriteKind.player)
-animation.run_image_animation(player1,
-    [img("""
-            . . . . . . f f f f . . . . . . 
-                . . . . f f f 2 2 f f f . . . . 
-                . . . f f f 2 2 2 2 f f f . . . 
-                . . f f f e e e e e e f f f . . 
-                . . f f e 2 2 2 2 2 2 e e f . . 
-                . . f e 2 f f f f f f 2 e f . . 
-                . . f f f f e e e e f f f f . . 
-                . f f e f b f 4 4 f b f e f f . 
-                . f e e 4 1 f d d f 1 4 e e f . 
-                . . f e e d d d d d d e e f . . 
-                . . . f e e 4 4 4 4 e e f . . . 
-                . . e 4 f 2 2 2 2 2 2 f 4 e . . 
-                . . 4 d f 2 2 2 2 2 2 f d 4 . . 
-                . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
-                . . . . . f f f f f f . . . . . 
-                . . . . . f f . . f f . . . . .
-        """),
-        img("""
-            . . . . . . . . . . . . . . . . 
-                . . . . . . f f f f . . . . . . 
-                . . . . f f f 2 2 f f f . . . . 
-                . . . f f f 2 2 2 2 f f f . . . 
-                . . f f f e e e e e e f f f . . 
-                . . f f e 2 2 2 2 2 2 e e f . . 
-                . f f e 2 f f f f f f 2 e f f . 
-                . f f f f f e e e e f f f f f . 
-                . . f e f b f 4 4 f b f e f . . 
-                . . f e 4 1 f d d f 1 4 e f . . 
-                . . . f e 4 d d d d 4 e f e . . 
-                . . f e f 2 2 2 2 e d d 4 e . . 
-                . . e 4 f 2 2 2 2 e d d e . . . 
-                . . . . f 4 4 5 5 f e e . . . . 
-                . . . . f f f f f f f . . . . . 
-                . . . . f f f . . . . . . . . .
-        """),
-        img("""
-            . . . . . . f f f f . . . . . . 
-                . . . . f f f 2 2 f f f . . . . 
-                . . . f f f 2 2 2 2 f f f . . . 
-                . . f f f e e e e e e f f f . . 
-                . . f f e 2 2 2 2 2 2 e e f . . 
-                . . f e 2 f f f f f f 2 e f . . 
-                . . f f f f e e e e f f f f . . 
-                . f f e f b f 4 4 f b f e f f . 
-                . f e e 4 1 f d d f 1 4 e e f . 
-                . . f e e d d d d d d e e f . . 
-                . . . f e e 4 4 4 4 e e f . . . 
-                . . e 4 f 2 2 2 2 2 2 f 4 e . . 
-                . . 4 d f 2 2 2 2 2 2 f d 4 . . 
-                . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
-                . . . . . f f f f f f . . . . . 
-                . . . . . f f . . f f . . . . .
-        """),
-        img("""
-            . . . . . . . . . . . . . . . . 
-                . . . . . . f f f f . . . . . . 
-                . . . . f f f 2 2 f f f . . . . 
-                . . . f f f 2 2 2 2 f f f . . . 
-                . . f f f e e e e e e f f f . . 
-                . . f e e 2 2 2 2 2 2 e f f . . 
-                . f f e 2 f f f f f f 2 e f f . 
-                . f f f f f e e e e f f f f f . 
-                . . f e f b f 4 4 f b f e f . . 
-                . . f e 4 1 f d d f 1 4 e f . . 
-                . . e f e 4 d d d d 4 e f . . . 
-                . . e 4 d d e 2 2 2 2 f e f . . 
-                . . . e d d e 2 2 2 2 f 4 e . . 
-                . . . . e e f 5 5 4 4 f . . . . 
-                . . . . . f f f f f f f . . . . 
-                . . . . . . . . . f f f . . . .
-        """)],
-    500,
-    True)
 player2 = sprites.create(img("""
         . . . . . f f 4 4 f f . . . . . 
             . . . . f 5 4 5 5 4 5 f . . . . 
@@ -386,81 +200,6 @@ player2 = sprites.create(img("""
             . . . . . f f b b f f . . . . .
     """),
     SpriteKind.player)
-animation.run_image_animation(player2,
-    [img("""
-            . . . . . f f 4 4 f f . . . . . 
-                . . . . f 5 4 5 5 4 5 f . . . . 
-                . . . f e 4 5 5 5 5 4 e f . . . 
-                . . f b 3 e 4 4 4 4 e 3 b f . . 
-                . . f 3 3 3 3 3 3 3 3 3 3 f . . 
-                . f 3 3 e b 3 e e 3 b e 3 3 f . 
-                . f 3 3 f f e e e e f f 3 3 f . 
-                . f b b f b f e e f b f b b f . 
-                . f b b e 1 f 4 4 f 1 e b b f . 
-                f f b b f 4 4 4 4 4 4 f b b f f 
-                f b b f f f e e e e f f f b b f 
-                . f e e f b d d d d b f e e f . 
-                . . e 4 c d d d d d d c 4 e . . 
-                . . e f b d b d b d b b f e . . 
-                . . . f f 1 d 1 d 1 d f f . . . 
-                . . . . . f f b b f f . . . . .
-        """),
-        img("""
-            . . . . . . . f f . . . . . . . 
-                . . . . . f f 4 4 f f . . . . . 
-                . . . . f 5 4 5 5 4 5 f . . . . 
-                . . . f e 4 5 5 5 5 4 e f . . . 
-                . . f b 3 e 4 4 4 4 e 3 b f . . 
-                . f e 3 3 3 3 3 3 3 3 3 3 e f . 
-                . f 3 3 e b 3 e e 3 b e 3 3 f . 
-                . f b 3 f f e e e e f f 3 b f . 
-                f f b b f b f e e f b f b b f f 
-                f b b b e 1 f 4 4 f 1 e b b b f 
-                . f b b e e 4 4 4 4 4 f b b f . 
-                . . f 4 4 4 e d d d b f e f . . 
-                . . f e 4 4 e d d d d c 4 e . . 
-                . . . f e e d d b d b b f e . . 
-                . . . f f 1 d 1 d 1 1 f f . . . 
-                . . . . . f f f b b f . . . . .
-        """),
-        img("""
-            . . . . . f f 4 4 f f . . . . . 
-                . . . . f 5 4 5 5 4 5 f . . . . 
-                . . . f e 4 5 5 5 5 4 e f . . . 
-                . . f b 3 e 4 4 4 4 e 3 b f . . 
-                . . f 3 3 3 3 3 3 3 3 3 3 f . . 
-                . f 3 3 e b 3 e e 3 b e 3 3 f . 
-                . f 3 3 f f e e e e f f 3 3 f . 
-                . f b b f b f e e f b f b b f . 
-                . f b b e 1 f 4 4 f 1 e b b f . 
-                f f b b f 4 4 4 4 4 4 f b b f f 
-                f b b f f f e e e e f f f b b f 
-                . f e e f b d d d d b f e e f . 
-                . . e 4 c d d d d d d c 4 e . . 
-                . . e f b d b d b d b b f e . . 
-                . . . f f 1 d 1 d 1 d f f . . . 
-                . . . . . f f b b f f . . . . .
-        """),
-        img("""
-            . . . . . . . f f . . . . . . . 
-                . . . . . f f 4 4 f f . . . . . 
-                . . . . f 5 4 5 5 4 5 f . . . . 
-                . . . f e 4 5 5 5 5 4 e f . . . 
-                . . f b 3 e 4 4 4 4 e 3 b f . . 
-                . f e 3 3 3 3 3 3 3 3 3 3 e f . 
-                . f 3 3 e b 3 e e 3 b e 3 3 f . 
-                . f b 3 f f e e e e f f 3 b f . 
-                f f b b f b f e e f b f b b f f 
-                f b b b e 1 f 4 4 f 1 e b b b f 
-                . f b b f 4 4 4 4 4 e e b b f . 
-                . . f e f b d d d e 4 4 4 f . . 
-                . . e 4 c d d d d e 4 4 e f . . 
-                . . e f b b d b d d e e f . . . 
-                . . . f f 1 1 d 1 d 1 f f . . . 
-                . . . . . f b b f f f . . . . .
-        """)],
-    100,
-    True)
 controller.player1.move_sprite(player1, 100, 100)
 controller.player2.move_sprite(player2, 100, 100)
 info.player1.set_life(3)
@@ -468,6 +207,26 @@ info.player2.set_life(3)
 info.player1.set_score(0)
 info.player2.set_score(0)
 scene.camera_follow_sprite(player1)
+Snake = sprites.create(img("""
+        . . . . c c c c c c . . . . . . 
+            . . . c 6 7 7 7 7 6 c . . . . . 
+            . . c 7 7 7 7 7 7 7 7 c . . . . 
+            . c 6 7 7 7 7 7 7 7 7 6 c . . . 
+            . c 7 c 6 6 6 6 c 7 7 7 c . . . 
+            . f 7 6 f 6 6 f 6 7 7 7 f . . . 
+            . f 7 7 7 7 7 7 7 7 7 7 f . . . 
+            . . f 7 7 7 7 6 c 7 7 6 f c . . 
+            . . . f c c c c 7 7 6 f 7 7 c . 
+            . . c 7 2 7 7 7 6 c f 7 7 7 7 c 
+            . c 7 7 2 7 7 c f c 6 7 7 6 c c 
+            c 1 1 1 1 7 6 f c c 6 6 6 c . . 
+            f 1 1 1 1 1 6 6 c 6 6 6 6 f . . 
+            f 6 1 1 1 1 1 6 6 6 6 6 c f . . 
+            . f 6 1 1 1 1 1 1 6 6 6 f . . . 
+            . . c c c c c c c c c f . . . .
+    """),
+    SpriteKind.enemy)
+Snake.set_position(142, 16)
 player1Health = statusbars.create(20, 4, StatusBarKind.health)
 player1Health.set_color(3, 2)
 player1Health.value = 100
@@ -493,63 +252,25 @@ def on_update_interval():
     # This creates multiple enemies and also limits the amount of enemies in the game
     while len(Enemies) < 7:
         Snake = sprites.create(img("""
-                . . . . . . c c c c c c . . . . 
-                            . . . . . c 6 7 7 7 7 6 c . . . 
-                            . . . . c 7 7 7 7 7 7 7 7 c . . 
-                            . . . c 6 7 7 7 7 7 7 7 7 6 c . 
-                            . . . c 7 7 7 c 6 6 6 6 c 7 c . 
-                            . . . f 7 7 7 6 f 6 6 f 6 7 f . 
-                            . . . f 7 7 7 7 7 7 7 7 7 7 f . 
-                            . . c f 6 7 7 c 6 7 7 7 7 f . . 
-                            . c 7 7 f 6 7 7 c c c c f . . . 
-                            c 7 7 7 7 f c 6 7 7 7 2 7 c . . 
-                            c c 6 7 7 6 c f c 7 7 2 7 7 c . 
-                            . . c 6 6 6 c c f 6 7 1 1 1 1 c 
-                            . . f 6 6 6 6 c 6 6 1 1 1 1 1 f 
-                            . . f c 6 6 6 6 6 1 1 1 1 1 6 f 
-                            . . . f 6 6 6 1 1 1 1 1 1 6 f . 
-                            . . . . f c c c c c c c c c . .
+                . . . . c c c c c c . . . . . . 
+                            . . . c 6 7 7 7 7 6 c . . . . . 
+                            . . c 7 7 7 7 7 7 7 7 c . . . . 
+                            . c 6 7 7 7 7 7 7 7 7 6 c . . . 
+                            . c 7 c 6 6 6 6 c 7 7 7 c . . . 
+                            . f 7 6 f 6 6 f 6 7 7 7 f . . . 
+                            . f 7 7 7 7 7 7 7 7 7 7 f . . . 
+                            . . f 7 7 7 7 6 c 7 7 6 f c . . 
+                            . . . f c c c c 7 7 6 f 7 7 c . 
+                            . . c 7 2 7 7 7 6 c f 7 7 7 7 c 
+                            . c 7 7 2 7 7 c f c 6 7 7 6 c c 
+                            c 1 1 1 1 7 6 f c c 6 6 6 c . . 
+                            f 1 1 1 1 1 6 6 c 6 6 6 6 f . . 
+                            f 6 1 1 1 1 1 6 6 6 6 6 c f . . 
+                            . f 6 1 1 1 1 1 1 6 6 6 f . . . 
+                            . . c c c c c c c c c f . . . .
             """),
             SpriteKind.enemy)
         Snake.set_position(randint(0, scene.screen_width()),
             randint(0, scene.screen_height()))
-        animation.run_image_animation(Snake,
-            [img("""
-                    . . . . . . c c c c c c . . . . 
-                                . . . . . c 6 7 7 7 7 6 c . . . 
-                                . . . . c 7 7 7 7 7 7 7 7 c . . 
-                                . . . c 6 7 7 7 7 7 7 7 7 6 c . 
-                                . . . c 7 7 7 c 6 6 6 6 c 7 c . 
-                                . . . f 7 7 7 6 f 6 6 f 6 7 f . 
-                                . . . f 7 7 7 7 7 7 7 7 7 7 f . 
-                                . . c f 6 7 7 c 6 7 7 7 7 f . . 
-                                . c 7 7 f 6 7 7 c c c c f . . . 
-                                c 7 7 7 7 f c 6 7 7 7 2 7 c . . 
-                                c c 6 7 7 6 c f c 7 7 2 7 7 c . 
-                                . . c 6 6 6 c c f 6 7 1 1 1 1 c 
-                                . . f 6 6 6 6 c 6 6 1 1 1 1 1 f 
-                                . . f c 6 6 6 6 6 1 1 1 1 1 6 f 
-                                . . . f 6 6 6 1 1 1 1 1 1 6 f . 
-                                . . . . f c c c c c c c c c . .
-                """),
-                img("""
-                    . . . . . . . c c c c c c . . . 
-                                . . . . . . c 6 7 7 7 7 6 c . . 
-                                . . . . . c 7 7 7 7 7 7 7 7 c . 
-                                . . . . c 6 7 7 7 7 7 7 7 7 6 c 
-                                . . . . c 7 7 7 c 6 6 6 6 c 7 c 
-                                . . . . f 7 7 7 6 f 6 6 f 6 7 f 
-                                . . . . f 7 7 7 7 7 7 7 7 7 7 f 
-                                . . . . f 6 7 7 c 6 7 7 7 7 f . 
-                                . . c c c f 6 7 7 c c c c f . . 
-                                . c 7 7 7 c c f 7 7 7 2 6 c . . 
-                                c 7 7 7 7 6 f c 7 7 2 7 7 6 c . 
-                                c c c 6 6 6 c 6 6 7 1 1 1 1 c . 
-                                . . c 6 6 6 6 6 6 1 1 1 1 1 c . 
-                                . . c 6 6 6 6 6 1 1 1 1 1 6 c . 
-                                . . c c 6 6 7 1 1 1 1 1 6 c . . 
-                                . . . c c c c c c c c c c . . .
-                """)],
-            500,
-            True)
+        Enemies = sprites.all_of_kind(SpriteKind.enemy)
 game.on_update_interval(100, on_update_interval)
